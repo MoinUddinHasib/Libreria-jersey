52d237f82df7c125c3ba5af74df9315f0df57ac7
Hasib <moinuddinhasib@gmail.com>
2020-12-18 15:45:13 +0100

    final commit

---
+++ src/test/java/it/solvingteam/jerseyLibreria/JerseyLibreriaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.solvingteam.jerseyLibreria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JerseyLibreriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
